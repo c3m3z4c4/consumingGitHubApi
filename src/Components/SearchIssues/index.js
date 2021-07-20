70fe70207112d90b2e30ca6492c3ee47023187d3
Cesar Meza <cmeza@mezadigital.com>
2021-07-20 16:04:16 -0500

    [ISSUE] setResult is not working propperly

--- src/Components/SearchIssues/index.js
+++ src/Components/SearchIssues/index.js
@@ -4,33 +4,19 @@ const SearchIssues = () => {
 
     const [search, setSearch] = useState('bug');
     const [text, setText] = useState('')
-    const [resultado, setResultado] = useState([]);
-    let arreglo = [];
+    const [resultado, setResultado] = useState({});
     
-    useEffect( () => {
-        getData();
-    },[])
-
-    const getData = async () => {
+    useEffect( async () => {
         const url = `https://api.github.com/search/issues?q=${search}`;
-        const resp = await fetch(url);
+        const resp =  await fetch(url);
         const data = await resp.json();
-        const issues = await data.items.map( issue => {
-            arreglo.push({
-                id: issue.id, 
-                title: issue.title, 
-                labels:issue.labels
-            })
-        })
-        console.log(arreglo)
-        setResultado(arreglo)
-    }
-
+        console.log(data)
+        setResultado(data);
+    },[]) //Here we look forward to change the request depending or the bug placed in search state
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        getData()
         console.log(text)
     }
 
@@ -42,7 +28,8 @@ const SearchIssues = () => {
             </form>
             <div>
                 <ul>
-                    {resultado.length > 1 ? resultado?.map((item) => (<li key={item.id}><span>{item.title}, {item.labels}</span></li>)) : <span>No Results</span>}
+                    {/* {resultado.length > 1 ? resultado?.map((item) => (<li key={item.id}><span>{item.title}, {item.labels}</span></li>)) : <span>No Results</span>} */}
+                    {resultado.items?.map((item) => (<li key={`items-${item.id}`}><span>{item.title}, {item.labels}</span></li>))}
                 </ul>
             </div>
         </div>
